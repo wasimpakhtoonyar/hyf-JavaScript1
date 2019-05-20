51e7562d13be75066a794fd4ab731bec634d5182
Marco Martins <marco.martins@lego.com>
2019-05-20 06:26:55 +0200

    JavaScript 1 week 1 notes

---
+++ week1/javascript.js
@@ -0,0 +1,29 @@
+let thisVarialbe = "21";
+let thisOtherVariable = 21;
+
+console.log(typeof thisVarialbe);
+console.log(thisVarialbe);
+
+console.log(typeof thisOtherVariable);
+console.log(thisOtherVariable);
+
+let arrayOfValues = ["1", 2, 3];
+console.log(typeof arrayOfValues);
+console.log(arrayOfValues);
+
+let objectOfValues = {name: 'Person'};
+console.log(typeof objectOfValues);
+console.log(objectOfValues);
+
+let thisIsFalse = false;
+if (thisIsFalse === false) {
+    console.log("yes, this is false");
+} else {
+    console.log("that was true");
+}
+console.log('it is still here');
+
+let thisNumber = 42;
+if (thisNumber !== 21) {
+    console.log('The number is not 21');
+}
