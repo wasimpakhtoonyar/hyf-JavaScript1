ac250d10559299fd6ea82f8dc60b7e7901c43e07
Marco Martins <marcodanielmartins@gmail.com>
2019-08-29 18:27:36 +0200

    week1

---
+++ phase-2/week1/arrays.js
@@ -0,0 +1,29 @@
+console.log("this is arrays");
+
+const names = ["Marco", "Martins"];
+
+console.log(names);
+
+const brands = ["nike", "adidas", "puma", "apple", "microsoft"]
+
+console.log(brands.length);
+console.log("string".length);
+console.log(typeof brands);
+
+const mixArray = [1, "name", "age", "100", 200]
+console.log(mixArray);
+
+const newArray = new Array();
+const anotherArray = [];
+
+console.log(newArray);
+console.log(brands);
+console.log(brands.indexOf("puma"));
+
+console.log(brands.indexOf("marvel"));
+
+console.log(brands[0]);
+
+brands[1] = "my brand";
+
+console.log(brands);
---
+++ phase-2/week1/javascript1.js
@@ -0,0 +1,27 @@
+console.log("now we are here");
+
+let size = "this is a string".length
+
+console.log(size);
+
+const lower = "Marco Martins"
+
+console.log(lower);
+console.log(lower.length);
+console.log(lower.toLowerCase());
+console.log(lower);
+
+let newWord = lower.toLowerCase();
+console.log(newWord.toUpperCase());
+console.log(newWord);
+
+if (typeof lower == typeof newWord) {
+  console.log("yes");  
+} else {
+    console.log("no");
+}
+
+const year = 2019;
+const newYear = year / 2;
+console.log(newYear);
+
